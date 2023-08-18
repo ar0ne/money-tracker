7ce082ea4dc6648069b58ca70c2c5d0a37c4086f
ar0ne <camtbitakou@yandex.ru>
2023-08-18 23:53:27 +0800

    fixed description

--- src/pages/app-home.ts
+++ src/pages/app-home.ts
@@ -2,7 +2,6 @@ import { LitElement, css, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { resolveRouterPath } from '../router';
 
-
 import '@shoelace-style/shoelace/dist/components/card/card.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 
