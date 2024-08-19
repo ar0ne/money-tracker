2278aee3ef6c89bf920697d38bd1609d43e5f33b
ar0ne <camtbitakou@yandex.ru>
2024-08-19 23:52:44 +0400

    added default values for categories and currencies

---
+++ src/domain/data.ts
@@ -0,0 +1,14 @@
+import { Currency, Category } from "./model";
+
+export const DEFAULT_CURRENCIES = [
+    new Currency("US Dollar", "$"),
+    new Currency("Euro", "€")
+];
+
+export const DEFAULT_CATEGORIES = [
+    new Category("Entertaiment"),
+    new Category("Groceries"),
+    new Category("Transport"),
+];
+
+
--- src/domain/db.ts
+++ src/domain/db.ts
@@ -1,5 +1,6 @@
+import { DEFAULT_CURRENCIES, DEFAULT_CATEGORIES } from "./data";
+
 let request: IDBOpenDBRequest;
-let db: IDBDatabase;
 let version = 1;
 const DB_NAME = 'expensesDB';
 
@@ -16,7 +17,7 @@ export const initDB = (): Promise<boolean|IDBDatabase> => {
 
     // if the data object store doesn't exist, create it
     request.onupgradeneeded = () => {
-      db = request.result;
+      var db = request.result;
 
       if (!db.objectStoreNames.contains(Stores.Expenses)) {
         console.log('Creating expenses store');
@@ -25,10 +26,12 @@ export const initDB = (): Promise<boolean|IDBDatabase> => {
       if (!db.objectStoreNames.contains(Stores.Categories)) {
         console.log('Creating categories store');
         db.createObjectStore(Stores.Categories, { keyPath: 'id' });
+        DEFAULT_CATEGORIES.forEach(category => addData(Stores.Categories, category));
       }
       if (!db.objectStoreNames.contains(Stores.Currencies)) {
         console.log('Creating currencies store');
         db.createObjectStore(Stores.Currencies, { keyPath: 'id' });
+        DEFAULT_CURRENCIES.forEach(currency => addData(Stores.Currencies, currency));
       }
       if (!db.objectStoreNames.contains(Stores.Settings)) {
         console.log('Creating settings store');
@@ -38,10 +41,10 @@ export const initDB = (): Promise<boolean|IDBDatabase> => {
     };
 
     request.onsuccess = (_e) => {
-      db = request.result;
+      var db = _e.target.result;
       // get current version and store it
       version = db.version;
-      resolve(request.result);
+      resolve(db);
     };
 
     request.onerror = (_e) => {
@@ -54,8 +57,8 @@ export const addData = <T>(storeName: string, data: T): Promise<T|string|null> =
   return new Promise((resolve) => {
     request = indexedDB.open(DB_NAME, version);
 
-    request.onsuccess = () => {
-      db = request.result;
+    request.onsuccess = (event) => {
+      var db = event.target.result;
       const tx = db.transaction(storeName, 'readwrite');
       const store = tx.objectStore(storeName);
       store.add(data);
@@ -77,8 +80,8 @@ export const deleteData = (storeName: string, key: string): Promise<boolean> => 
   return new Promise((resolve) => {
     request = indexedDB.open(DB_NAME, version);
 
-    request.onsuccess = () => {
-      db = request.result;
+    request.onsuccess = (event) => {
+      db = event.target.result;
       const tx = db.transaction(storeName, 'readwrite');
       const store = tx.objectStore(storeName);
       const res = store.delete(key);
@@ -96,8 +99,8 @@ export const updateData = <T>(storeName: string, key: string, data: T): Promise<
   return new Promise((resolve) => {
     request = indexedDB.open(DB_NAME, version);
 
-    request.onsuccess = () => {
-      db = request.result;
+    request.onsuccess = (event) => {
+      var db = event.target.result;
       const tx = db.transaction(storeName, 'readwrite');
       const store = tx.objectStore(storeName);
       const res = store.get(key);
@@ -117,8 +120,8 @@ export const getStoreData = <T>(storeName: Stores): Promise<T[]> => {
   return new Promise((resolve) => {
     request = indexedDB.open(DB_NAME);
 
-    request.onsuccess = () => {
-      db = request.result;
+    request.onsuccess = (event) => {
+      var db = event.target.result;
       const tx = db.transaction(storeName, 'readonly');
       const store = tx.objectStore(storeName);
       const res = store.getAll();
@@ -133,8 +136,8 @@ export const getStoreDataById = <T>(storeName: Stores, id: string): Promise<T|un
     return new Promise((resolve) => {
       request = indexedDB.open(DB_NAME);
 
-      request.onsuccess = () => {
-        db = request.result;
+      request.onsuccess = (event) => {
+        var db = event.target.result;
         const tx = db.transaction(storeName, 'readonly');
         const store = tx.objectStore(storeName);
         const res = store.get(id);
