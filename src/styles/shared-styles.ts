26d85ecafe9fb7fb60fdf7f3aa3339a72ff182b5
ar0ne <camtbitakou@yandex.ru>
2024-08-20 16:59:31 +0400

    fixed font color for white theme

--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -26,37 +26,54 @@ export const styles = css`
     display: none;
   }
   .color-0 {
-    background-color: #D5DEE5;
+    background-color: #D0E1D4;
+    padding: 0.1em;
   }
   .color-1 {
     background-color: #33658A;
+    padding: 0.1em;
   }
   .color-2 {
-    background-color: #5D90B1;
+    background-color: #FFE2FE;
+    padding: 0.1em;
   }
   .color-3 {
-    background-color: #86BBD8;
+    background-color: #71697A;
+    padding: 0.1em;
   }
   .color-4 {
     background-color: #5B8298;
+    padding: 0.1em;
   }
   .color-5 {
     background-color: #2F4858;
+    padding: 0.1em;
   }
   .color-6 {
     background-color: #937B43;
+    padding: 0.1em;
   }
   .color-7 {
-    background-color: #F6AE2D;
+    background-color: #003459;
+    padding: 0.1em;
   }
   .color-8 {
     background-color: #F26419;
+    padding: 0.1em;
   }
   .color-9 {
-    background-color: #F3722E;
+    background-color: #9CEAEF;
+    padding: 0.1em;
   }
   .color-10 {
-    background-color: #F47F41;
+    background-color: #8C271E;
+    padding: 0.1em;
+  }
+
+  @media (prefers-color-scheme: light) {
+    .color-1, .color-3, .color-4, .color-5, .color-6, .color-7, .color-8, .color-10 {
+        color: white;
+    }
   }
 
 `;
