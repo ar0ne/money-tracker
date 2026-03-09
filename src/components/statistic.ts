fde901660c5087e28ece56419197d2e86ca2e79d
ar0ne <camtbitakou@yandex.ru>
2026-03-09 22:27:09 +0530

    fixed style

--- src/components/statistic.ts
+++ src/components/statistic.ts
@@ -24,9 +24,6 @@ class AppStatistic extends LitElement {
                     margin-left: auto;
                     margin-right: auto;
                 }
-                .stat-category {
-                    margin-left: -1em;
-                }
                 .stat-list {
                     display: inline-block;
                 }
